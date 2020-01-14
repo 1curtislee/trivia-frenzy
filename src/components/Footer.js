8fd80b41b19e26715764a717c6af33fa17c437c0
1curtislee <1curtislee@gmail.com>
2020-01-14 13:42:26 -0800

    basic quiz functionality

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div className="App-footer">
+      <p>© 2020 Trivia Frenzy</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div className="App-header">
+      <a href="/">
+        <button>HOME</button>
+      </a>
+
+      <h2>Trivia Frenzy</h2>
+    </div>
+  );
+}
+
+export default Header;
